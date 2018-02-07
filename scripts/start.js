d822ec17631b53b4f7db79dc1374f6fccb175927
Luiz Henrique Almeida da Silva <luizhrqas@gmail.com>
2018-02-07 14:21:36 -0200

    stopped work

--- scripts/start.js
+++ scripts/start.js
@@ -1,4 +1,25 @@
 const app = require("../src/index.js");
+const nodemon = require("nodemon");
 
-console.log("Moviez backend started! :)");
+process.on("unhandledRejection", err => {
+  throw err;
+});
+
 app.listen(3001);
+
+nodemon({
+  script: "src/index.js",
+  ext: "js json"
+});
+
+nodemon
+  .on("start", function() {
+    console.log("Moviez started! :)");
+  })
+  .on("quit", function() {
+    console.log("Moviez has quit! :(");
+    process.exit();
+  })
+  .on("restart", function(files) {
+    console.log("Moviez restarted due to: ", files);
+  });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
-
+const readTorrent = require("read-torrent");
 const app = express();
+
+app.post("/create", (req, res) => {
+  const { magnet } = req.body;
+  readTorrent(magnet, onFinish);
+
+  const onFinish = (err, torrent) => {
+    if (err) return res.send({ error: true, content: err });
+    const infoHash = torrent.infoHash;
+  };
+  res.send("ok!");
+});
 
 module.exports = app;
